24fddc1bf14e5037fa8fef856ae2fbf2c36b8dea
Raito <145450869+Q7nurs3vim@users.noreply.github.com>
2023-10-30 23:55:23 +0300

    Add files via upload
    Evdeki bazı sistemleri kontrol eden proje, , daha da geliştirecegim.  :)

---
+++ akıllı-ev-1575/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace akıllı_ev_1575
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            groupBox1 = new GroupBox();
+            button1 = new Button();
+            checkBox2 = new CheckBox();
+            checkBox1 = new CheckBox();
+            groupBox2 = new GroupBox();
+            listBox1 = new ListBox();
+            groupBox1.SuspendLayout();
+            groupBox2.SuspendLayout();
+            SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            groupBox1.BackColor = Color.MistyRose;
+            groupBox1.Controls.Add(button1);
+            groupBox1.Controls.Add(checkBox2);
+            groupBox1.Controls.Add(checkBox1);
+            groupBox1.Location = new Point(21, 12);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(250, 198);
+            groupBox1.TabIndex = 0;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Sistem kontrolü";
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.Chartreuse;
+            button1.Location = new Point(16, 152);
+            button1.Name = "button1";
+            button1.Size = new Size(197, 29);
+            button1.TabIndex = 2;
+            button1.Text = "Sİstemi kontrol et";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // checkBox2
+            // 
+            checkBox2.AutoSize = true;
+            checkBox2.Location = new Point(25, 103);
+            checkBox2.Name = "checkBox2";
+            checkBox2.Size = new Size(170, 24);
+            checkBox2.TabIndex = 1;
+            checkBox2.Text = "Kombi açık AÇ/KAPA";
+            checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Location = new Point(25, 40);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(171, 24);
+            checkBox1.TabIndex = 0;
+            checkBox1.Text = "Lamba açık AÇ/KAPA";
+            checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            groupBox2.BackColor = Color.MistyRose;
+            groupBox2.Controls.Add(listBox1);
+            groupBox2.Location = new Point(291, 12);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(250, 198);
+            groupBox2.TabIndex = 1;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "Sistem Raporu";
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 20;
+            listBox1.Location = new Point(26, 29);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(198, 144);
+            listBox1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ActiveCaption;
+            ClientSize = new Size(565, 231);
+            Controls.Add(groupBox2);
+            Controls.Add(groupBox1);
+            Name = "Form1";
+            Text = "Form1";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            groupBox2.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private GroupBox groupBox1;
+        private Button button1;
+        private CheckBox checkBox2;
+        private CheckBox checkBox1;
+        private GroupBox groupBox2;
+        private ListBox listBox1;
+    }
+}
---
+++ akıllı-ev-1575/Form1.cs
@@ -0,0 +1,35 @@
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace akıllı_ev_1575
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked == true)
+            {
+                listBox1.Items.Add("Lambalar Açık");
+            }
+            else
+            {
+                listBox1.Items.Add("Lambalar Kapalı");
+            }
+            if (checkBox2.Checked == true)
+            {
+                listBox1.Items.Add("Kombi Açık");
+            }
+            else
+            {
+                listBox1.Items.Add("Kombi Kapalı");
+            }
+            
+
+        }
+    }
+}
